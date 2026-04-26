8ca7838f119f302d5683500f1289a9765858b084
NITI <87989963+0014niti@users.noreply.github.com>
2026-04-26 21:44:12 +0530

    Update ex-htx.ts

--- src/lib/exchanges/ex-htx.ts
+++ src/lib/exchanges/ex-htx.ts
@@ -17,8 +17,17 @@ export const fetchHtx = async (props: { type: 'buy' | 'sell'; token: string; fia
 	try {
 		const res = await fetch(targetUrl, {
 			headers: { 
-				'Accept': 'application/json',
-				'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'
+				'Accept': 'application/json, text/plain, */*',
+				'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
+				'Accept-Language': 'en-US,en;q=0.9',
+				'Origin': 'https://www.htx.com',
+				'Referer': 'https://www.htx.com/en-us/fiat-crypto/trade/buy-usdt-usd/',
+				'Sec-Ch-Ua': '"Chromium";v="124", "Google Chrome";v="124", "Not-A.Brand";v="99"',
+				'Sec-Ch-Ua-Mobile': '?0',
+				'Sec-Ch-Ua-Platform': '"Windows"',
+				'Sec-Fetch-Dest': 'empty',
+				'Sec-Fetch-Mode': 'cors',
+				'Sec-Fetch-Site': 'same-origin'
 			}
 		});
 		if (res.ok) {
